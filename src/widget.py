3ccbe82a268e3ee06bc692df5f56bf916ba7f51b
Mikail Makarov <mishamakarov@bk.ru>
2024-07-07 00:08:27 +0300

    add widget.py andnew func

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(card_info: str) -> str:
+    """Функция, которая принимает номер или счет карты и возвращает маску"""
+
+
+def get_date(input_data: str) -> str:
+    """Функция, которая меняет формат получаемой даты на "ДД.ММ.ГГГГ" """
+
+
+
+
+
+
+
+
+
+

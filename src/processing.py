e31b7f7b1b07558fcc021b3e750c0c201953b230
Mikail Makarov <mishamakarov@bk.ru>
2024-07-12 15:39:55 +0300

    add new functions

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+
+def filter_by_state(input_list: list[dict[str, Any]], state_id: str = "EXECUTED") -> list[dict[str, Any]]:
+    """Функция, сортирующая список по указанному ключу"""
+    pass
+
+
+def sort_by_date(input_list: list[dict[str, Any]], reverse=True) -> list[dict[str, Any]]:
+    """Функция сортировки исходных данных по дате"""
+    pass
